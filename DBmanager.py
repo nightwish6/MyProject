4778fc73e8592a70d227fb08a3cf2a715776099b
Nightwish6 <nightwish6@mail.ru>
2017-05-13 18:25:17 +0300

    My 2 version DB manager

--- DBmanager.py
+++ DBmanager.py
@@ -2,19 +2,16 @@ import sqlite3
 
 class DBmanager(object):
     def __init__(self, name_db):
-        self.name=name_db
-        self.conn=sqlite3.connect(self.name)
+        self.conn=sqlite3.connect(name_db)
         self.dbcursor=self.conn.cursor()
-        self.dbcursor.execute('CREATE TABLE words (id int, word txt)')
+        self.dbcursor.execute('CREATE TABLE words (id int NOT NULL, word txt NOT NULL, PRIMARY KEY(id))')
     def insert_into(self, *values):
         list_val=list(values)
         self.dbcursor.executemany('INSERT INTO words VALUES(?,?)',list_val)
         self.conn.commit()
     def select_all_values(self):
-        all_values=[]
-        for row in self.dbcursor.execute('SELECT * FROM words'):
-            all_values.append(row)
-        return all_values
+        self.dbcursor.execute('SELECT * FROM words')
+        return self.dbcursor.fetchall()
     def select_last_value(self):
         self.dbcursor.execute('SELECT word FROM words WHERE id=(SELECT MAX(id) FROM words)')
         return self.dbcursor.fetchone()
@@ -24,8 +21,6 @@ class DBmanager(object):
     def del_last_value(self):
         self.dbcursor.execute('DELETE FROM words WHERE id=(SELECT MAX(id) FROM words)')
         self.conn.commit()
-
-
 
 
 
