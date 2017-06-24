21ca10828a7b748212695fd59a46dbb045706f2d
Nightwish6 <nightwish6@mail.ru>
2017-06-24 12:45:30 +0300

    Доработанная БД

--- db_manag.py
+++ db_manag.py
@@ -4,23 +4,60 @@ class DBmanager(object):
     def __init__(self, name_db):
         self.conn=sqlite3.connect('/home/admin1/GitProject/%s.sqlite'%(name_db))
         self.dbcursor=self.conn.cursor()
-        self.dbcursor.execute('CREATE TABLE words (id int NOT NULL, word txt NOT NULL, PRIMARY KEY(id))')
-    def insert_into(self, *values):
-        list_val=list(values)
-        self.dbcursor.executemany('INSERT INTO words VALUES(?,?)',list_val)
+    def create_table(self):
+        self.dbcursor.execute('CREATE TABLE valute (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,'
+                              'date txt NOT NULL,'
+                              'charcode txt NOT NULL, '
+                              'name txt NOT NULL,'
+                              'nominal int NOT NULL,'
+                              'numcode int NOT NULL,'
+                              'value int NOT NULL)')
+    def insert_into(self, data):
+        values = list()
+        for item in list(data.values()):
+            if len(item.keys()) > 2:
+                values.append((data['ValCurs']['Date'], item['CharCode'], item['Name'], item['Nominal'],
+                               item['NumCode'], item['Value']))
+        self.dbcursor.executemany('INSERT INTO valute(date, charcode, name, nominal, numcode, value)'
+                                  ' VALUES(?,?,?,?,?,?)',values)
         self.conn.commit()
     def select_all_values(self):
-        self.dbcursor.execute('SELECT * FROM words')
+        self.dbcursor.execute('SELECT * FROM valute')
         return self.dbcursor.fetchall()
-    def select_last_value(self):
-        self.dbcursor.execute('SELECT word FROM words WHERE id=(SELECT MAX(id) FROM words)')
+    def select_date_char_value(self, date, charcode):
+        date=date.replace('/','.')
+        self.dbcursor.execute('SELECT * FROM valute WHERE date=? and charcode=?', (date, charcode))
         return self.dbcursor.fetchone()
-    def del_all_values(self):
-        self.dbcursor.execute('DELETE FROM words')
+    def select_all_date_value(self, date):
+        date=date.replace('/','.')
+        self.dbcursor.execute('SELECT * FROM valute WHERE date=?',(date,))
+        return self.dbcursor.fetchall()
+    def del_all_date_value(self, date):
+        date=date.replace('/','.')
+        self.dbcursor.execute('DELETE FROM valute WHERE date=?', (date,))
         self.conn.commit()
-    def del_last_value(self):
-        self.dbcursor.execute('DELETE FROM words WHERE id=(SELECT MAX(id) FROM words)')
-        self.conn.commit()
+
+
+if __name__=='__main__':
+    a=DBmanager('valute')
+    from cb_requests import req_curr_rate
+    b=req_curr_rate('03/10/2013')
+    from pprint import pprint
+    #a.create_table()
+    #a.insert_into(b)
+    pprint(a.select_all_values())
+    #pprint(a.select_date_char_value('11/05/2017','CAD'))
+    #pprint(a.select_all_date_value('03/10/2013'))
+    #a.del_all_date_value('03/10/2013')
+
+
+
+
+
+
+
+
+
 
 
 
